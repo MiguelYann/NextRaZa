133f48a41f0a44141ba19f4ebe8a5b87a06aa3b9
Miguel Yann <tsambamiguelito@gmail.com>
2020-10-24 23:58:12 +0200

    feat: Store user to firestore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HomePage from './ui/pages/home/home.component';
 import { Switch, Route } from 'react-router-dom';
 import NavigationBar from './ui/resources/components/navigation-bar/navigation-bar.component';
-import  Hats  from './ui/pages/hats/hats.component';
+import Hats from './ui/pages/hats/hats.component';
 import { Shop } from './ui/pages/shop/shop.component';
 import SignInAndSignUp from './ui/pages/signIn-signUp/signin-signup.component';
+import { auth, createUserDocument } from './services/firebase/firebase-services';
 
-function App() {
-  return (
-    <div className="App">
-      <NavigationBar />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={Hats}/> 
-        <Route path="/shop" component={Shop} />
-        <Route path="/signin" component={SignInAndSignUp}/>
-      </Switch>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  state = {
+    currentUser: '',
+  };
+
+  disconnectFirebase = null;
+
+  componentDidMount() {
+    auth.onAuthStateChanged( async (user) => createUserDocument(user));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={Hats} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
--- src/services/firebase/firebase-services.js
+++ src/services/firebase/firebase-services.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 import { configurationFirebase } from './firebase-configuration';
 require('firebase/auth');
 
@@ -6,12 +6,24 @@ require('firebase/auth');
 firebase.initializeApp(configurationFirebase);
 
 export const auth = firebase.auth();
-const providerGoogle = new firebase.auth.GoogleAuthProvider()
-providerGoogle.setCustomParameters({prompt: 'select_account'});
+const providerGoogle = new firebase.auth.GoogleAuthProvider();
+providerGoogle.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(providerGoogle);
 
 //FireStore
 
 const firestoreRepository = firebase.firestore();
 
+export const createUserDocument = async (currentUser) => {
+  const queryUser = await firestoreRepository.doc('users/124kfgg');
+  const snapShotUser = await queryUser.get();
 
+  const { email, uid, photoURL, displayName } = currentUser;
+  if (snapShotUser.exists) {
+    return;
+  }
+
+  await queryUser.set({ email, uid, photoURL, displayName });
+
+  return queryUser;
+};
