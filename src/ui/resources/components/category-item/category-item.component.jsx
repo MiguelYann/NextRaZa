ce9ecdc8fc1703f96c4b5dac8fb1045c4be3e9fd
Miguel Yann <tsambamiguelito@gmail.com>
2020-10-25 03:31:52 +0100

    refactor: Remove text buy on home

--- src/ui/resources/components/category-item/category-item.component.jsx
+++ src/ui/resources/components/category-item/category-item.component.jsx
@@ -18,7 +18,6 @@ const goToSection = () =>  {
     >
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">ACHETER</span>
       </div>
     </div>
   );
